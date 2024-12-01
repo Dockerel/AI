b1a0eccce17699a8c2e710fd5d090492868ad096
Giheon Do <dgh0001@naver.com>
2024-12-01 11:30:26 +0900

    refactor: 유사 문서 검색 기능 리펙터링

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
 from pinecone import Pinecone
-from webEmbeddingPipeline import WebEmbeddingPipeline
 from sentence_transformers import SentenceTransformer
+from webEmbeddingPipeline import WebEmbeddingPipeline
+from queryProcessingPipeline import QueryProcessingPipeline
 
 load_dotenv()
 
@@ -13,11 +14,16 @@ pinecone_index_name = os.environ.get("PINECONE_INDEX_NAME")
 pc = Pinecone(api_key=pinecone_api_key)
 index = pc.Index(pinecone_index_name)
 
-# device = "cuda"
-# model = SentenceTransformer("all-MiniLM-L6-v2", device=device)
+# # device = "cuda"
+# # model = SentenceTransformer("all-MiniLM-L6-v2", device=device)
 
 device = "cpu"
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-embedding_pipeline = WebEmbeddingPipeline(index, model)
-embedding_pipeline.run()
+# # embedding pipeline
+# # embedding_pipeline = WebEmbeddingPipeline(index, model)
+# # embedding_pipeline.run()
+
+# query processing pipeline
+query_pipeline = QueryProcessingPipeline(index, model)
+query_pipeline.run("글솝인데 졸업 요건을 알고 싶어")
